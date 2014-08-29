8ac6cfb644e64569800333987147c5b378546fd2
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-29 03:00:56 -0700

    (cleanup) Moved not-so-misc aliases to their own files.

---
+++ runcoms/zshmodules/c.zsh
@@ -0,0 +1,4 @@
+# gcc/g++ aliases
+alias g++='g++-4.9'
+alias gcc='gcc-4.9'
+
--- runcoms/zshmodules/misc.zsh
+++ runcoms/zshmodules/misc.zsh
@@ -1,9 +1,3 @@
-eval "$(rbenv init -)"
-
-# gcc/g++ aliases
-alias g++='g++-4.9'
-alias gcc='gcc-4.9'
-
 # Servering aliases
 alias serve="python -m SimpleHTTPServer"
 
