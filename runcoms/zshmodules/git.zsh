4237de1883ead1e84b2af2cc221f29a1b0332a54
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-29 15:23:33 -0700

    (feat) Added new git utilities. Made existing utilites more robust.

--- runcoms/zshmodules/git.zsh
+++ runcoms/zshmodules/git.zsh
@@ -22,10 +22,49 @@ alias gpom='git push origin master'
 alias gpr="git pull-request"
 
 clone () {
+    if [ $# -lt 1 ]; then
+        echo "Usage: clone <REMOTE> <PLACE_HERE?>"
+        return 1
+    fi
+
     local get_this=$1
     local temp=${2-$1}
     local put_it_here=$(basename $temp)
     git clone $get_this $put_it_here
     cd $put_it_here
+}
+
+create () {
+    if [ $# -gt 0 ]; then
+        echo "Usage (when in a git repo): create"
+        return 1
+    fi
+
+    git status 2> /dev/null
+    if [ $? -eq 0 ]; then
+        git create
+        travis enable
+        git push origin master
+    else
+        echo "fatal: Not a git repository"
+        return 1
+    fi
+}
+
+gprq () {
+    if [ $# -lt 2 ]; then
+        echo "Usage: gprq <TO> <FROM> <MESSAGE?>"
+        return 1
+    fi
+
+    local to=$1
+    local from=$2
+    local current_branch=`git rev-parse --abbrev-ref HEAD`
+    if [ $# -gt 2 ]; then
+        local message=$3
+        gpr -h "$to:master" -b "$from:$current_branch" -m "$message"
+    else
+        gpr -h "$to:master" -b "$from:$current_branch"
+    fi
 }
 
