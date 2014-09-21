778cbdf8029e19f2c73facafb6ee9aae87fad4ec
Sean Chen <skypemaster007@gmail.com>
2014-09-20 18:10:17 -0700

    Updated PATH and EDITORs.

--- runcoms/zshmodules/vars.zsh
+++ runcoms/zshmodules/vars.zsh
@@ -1,9 +1,9 @@
 # Env Vars
-export PATH=/usr/local/bin:/usr/local/opt/ccache/libexec:/Users/Jonathan/Library/Haskell/bin:$PATH:/Users/Jonathan/.cabal/bin:/Users/Jonathan/.rvm/gems/ruby-2.0.0-p247/bin:/Users/Jonathan/.rvm/gems/ruby-2.0.0-p247@global/bin:/Users/Jonathan/.rvm/rubies/ruby-2.0.0-p247/bin:/Users/Jonathan/.rvm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin
 export LD_LIBRARY_PATH=usr/local/lib
-export GIT_EDITOR=/usr/local/bin/vim
-export EDITOR=/usr/local/bin/vim
-export VISUAL=/usr/local/bin/vim
+export GIT_EDITOR=/usr/local/bin/subl
+export EDITOR=/usr/local/bin/subl
+export VISUAL=/usr/local/bin/subl
 export NODE_PATH=/usr/local/lib/node_modules
 export NODE_ENV="development"
 export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
